88eef654a9dc4d085d65a082c8e8096a978b6072
LeoSanchezMiranda <ldjamesl@hotmail.com>
2015-10-21 13:39:53 -0500

    Updated Title
    This is a test

--- Koffee.WebAPI/Koffee.WebAPI/Controllers/HomeController.cs
+++ Koffee.WebAPI/Koffee.WebAPI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Koffee.WebAPI.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Kofee Kult";
 
             return View();
         }
