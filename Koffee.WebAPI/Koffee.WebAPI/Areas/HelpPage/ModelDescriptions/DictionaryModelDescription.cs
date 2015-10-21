30cfed8e2e74256e89e22dcb60abbef1453bfad5
Thomas Sobieck <thomas@sobieck.com>
2015-10-21 13:35:23 -0500

    First commit.

---
+++ Koffee.WebAPI/Koffee.WebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Koffee.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Koffee.WebAPI/Koffee.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Koffee.WebAPI.WebApiApplication" Language="C#" %>
